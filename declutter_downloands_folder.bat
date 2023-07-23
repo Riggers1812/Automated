3c1ae126df5bc0dd985edc7466c3ee115c2445ee
Riggers1812 <dtrigby@gmail.com>
2023-07-23 15:21:04 +0100

    creates Bat File

---
+++ declutter_downloands_folder.bat
@@ -0,0 +1,3 @@
+"E:\Users\dtrig\Documents\Coding\Learning\Automated\Organising_Downloads_Folder.py"
+"E:\Users\dtrig\Documents\Coding\Learning\Automated\Organising_Download_Documents_Folder.py"
+pause
