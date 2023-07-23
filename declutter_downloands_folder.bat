8e057745936455f1b524b72fc7dbb385ff98d3ea
David Rigby <31040493+Riggers1812@users.noreply.github.com>
2023-07-23 15:32:37 +0100

    Delete declutter_downloands_folder.bat

--- declutter_downloands_folder.bat
+++
@@ -1,3 +0,0 @@
-"E:\Users\dtrig\Documents\Coding\Learning\Automated\Organising_Downloads_Folder.py"
-"E:\Users\dtrig\Documents\Coding\Learning\Automated\Organising_Download_Documents_Folder.py"
-pause
